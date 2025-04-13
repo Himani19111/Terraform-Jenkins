ece84ae40e175e8941eb20fa67c8292e3941d0b7
Sandeep Kumar <89197845+Himani19111@users.noreply.github.com>
2025-04-13 20:20:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,3 +9,20 @@ resource "aws_instance" "foo" {
       Name = "TF-Instance"
   }
 }
+# ➕ Additional EBS Volume (20GB)
+resource "aws_ebs_volume" "extra_disk" {
+  availability_zone = aws_instance.my_instance.availability_zone
+  size              = 20  # Size in GB
+  type              = "gp2"  # General Purpose SSD
+
+  tags = {
+    Name = "Extra-20GB-Disk"
+  }
+}
+
+# 🔗 Attach the EBS Volume to the instance
+resource "aws_volume_attachment" "ebs_attach" {
+  device_name = "/dev/sdf"  # This is how Linux will see it
+  volume_id   = aws_ebs_volume.extra_disk.id
+  instance_id = aws_instance.my_instance.id
+}
